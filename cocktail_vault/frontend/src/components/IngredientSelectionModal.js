ff067613e675c38172456a42f641cee71ebcfce6
albireo <yunobo@protonmail.ch>
2021-06-04 14:38:47 +0900

    add API get request for inventory

--- cocktail_vault/frontend/src/components/IngredientSelectionModal.js
+++ cocktail_vault/frontend/src/components/IngredientSelectionModal.js
@@ -1,51 +1,27 @@
 import React from "react";
+import { useApi } from "../hooks/use-api";
 import SearchBar from "./SearchBar";
 import IngredientItem from "./IngredientItem";
 import "./IngredientSelectionModal.css";
 
-const ingredients = [
-  {
-    id: 0,
-    name: "Rum",
-    hasKind: true,
-    kind: "Dark",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 1,
-    name: "Coke",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 2,
-    name: "Vodka",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 3,
-    name: "Sugar",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-];
-
-function IngredientSelectionModal() {
+function IngredientSelectionModal({ onClose }) {
+  const api_url = "http://localhost:8000/api/get_all_ingredients";
+  const { data: ingredients } = useApi(api_url, false);
   return (
     <div className="IngredientSelectionModal">
       <div className="IngredientSelectionModal-content">
-        <SearchBar placeholder="Search Ingredients.." />
-        <h2>Ingredients</h2>
-        <ul className="IngredientSelectionModal-list">
-          {ingredients.map((ingredient) => (
-            <IngredientItem ingredient={ingredient} key={ingredient.id} />
-          ))}
-        </ul>
-        <button>Done</button>
+        {ingredients && (
+          <>
+            <SearchBar placeholder="Search Ingredients.." />
+            <h2>Ingredients</h2>
+            <ul className="IngredientSelectionModal-list">
+              {ingredients.map((ingredient) => (
+                <IngredientItem ingredient={ingredient} key={ingredient.id} />
+              ))}
+            </ul>
+            )<button onClick={onClose}>Done</button>
+          </>
+        )}
       </div>
     </div>
   );
--- cocktail_vault/frontend/src/components/Inventory.js
+++ cocktail_vault/frontend/src/components/Inventory.js
@@ -1,61 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Navbar from "./Navbar";
 import InventoryItem from "./InventoryItem";
 import IngredientSelectionModal from "./IngredientSelectionModal";
 import AddIcon from "@material-ui/icons/Add";
+import { useApi } from "../hooks/use-api";
 import "./Inventory.css";
 
-const ingredients = [
-  {
-    id: 0,
-    name: "Rum",
-    hasKind: true,
-    kind: "Dark",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 1,
-    name: "Coke",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 2,
-    name: "Vodka",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: 3,
-    name: "Sugar",
-    hasKind: false,
-    kind: "",
-    image: "https://picsum.photos/200",
-  },
-];
-
 function Inventory() {
+  const history = useHistory();
+  const api_url = "http://localhost:8000/api/get_inventory";
+  const { data: inventory } = useApi(api_url, true);
+  const [isModalShown, setIsModalShown] = useState(false);
   return (
     <>
       <div className="Inventory">
         <Navbar />
         <h2>Your Inventory</h2>
         <hr />
-        <ul className="Inventory-list">
-          {ingredients.map((ingredient) => (
-            <InventoryItem ingredient={ingredient} key={ingredient.id} />
-          ))}
-        </ul>
-        <ul className="Inventory-buttons">
-          <button>
-            <AddIcon /> Add Ingredient
-          </button>
-          <button>Your Recipes</button>
-        </ul>
+        {inventory && (
+          <>
+            <ul className="Inventory-list">
+              {inventory.map((ingredient) => (
+                <InventoryItem ingredient={ingredient} key={ingredient.id} />
+              ))}
+            </ul>
+            <ul className="Inventory-buttons">
+              <button onClick={() => setIsModalShown(true)}>
+                <AddIcon /> Add Ingredient
+              </button>
+              <button onClick={() => history.push("/explore")}>
+                Your Recipes
+              </button>
+            </ul>
+          </>
+        )}
       </div>
-      <IngredientSelectionModal />
+      {isModalShown && (
+        <IngredientSelectionModal onClose={() => setIsModalShown(false)} />
+      )}
     </>
   );
 }
