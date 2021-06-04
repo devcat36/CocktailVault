4e85c17fda7150f3942d0e43d86566510afa4274
albireo <yunobo@protonmail.ch>
2021-06-04 22:52:49 +0900

    remove unused import

--- cocktail_vault/frontend/src/components/IngredientSelectionModal.js
+++ cocktail_vault/frontend/src/components/IngredientSelectionModal.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useApi } from "../hooks/use-api";
 import { useToken } from "../hooks/use-token";
 import { addIngredient, removeIngredient } from "../api";
