5199dccf9537fa1a53a41aedf1cc428ef2179e5b
albireo <yunobo@protonmail.ch>
2021-06-06 21:55:27 +0900

    support api url change

--- cocktail_vault/frontend/src/hooks/use-api.js
+++ cocktail_vault/frontend/src/hooks/use-api.js
@@ -2,29 +2,31 @@ import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const useApi = (url, isProtected) => {
-  const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
   const [state, setState] = useState({
     error: null,
     loading: true,
     data: null,
   });
   const [refreshIndex, setRefreshIndex] = useState(0);
+  const [apiUrl, setApiUrl] = useState(url);
+
   const audience = "cocktail_vault";
   useEffect(() => {
     (async () => {
       try {
-        const accessToken = isProtected
+        const accessToken = (isProtected && isAuthenticated)
           ? await getAccessTokenSilently({ audience })
           : null;
         const authHeader = {
           audience,
           Authorization: `Bearer ${accessToken}`,
         };
-        const res = isProtected
-          ? await fetch(url, {
+        const res = (isProtected && isAuthenticated)
+          ? await fetch(apiUrl, {
               headers: authHeader,
             })
-          : await fetch(url);
+          : await fetch(apiUrl);
         setState({
           ...state,
           data: await res.json(),
@@ -39,10 +41,11 @@ export const useApi = (url, isProtected) => {
         });
       }
     })();
-  }, [refreshIndex]);
+  }, [refreshIndex, apiUrl]);
 
   return {
     ...state,
     refresh: () => setRefreshIndex(refreshIndex + 1),
+    setUrl : (url) => setApiUrl(url)
   };
 };
