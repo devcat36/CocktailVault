ecb44fcb95e238b04b58740b5079e6d17f8bbdb2
albireo <yunobo@protonmail.ch>
2021-06-06 21:56:08 +0900

    implement API request for cocktail recipe details

--- cocktail_vault/frontend/src/components/CocktailDetailsPage.js
+++ cocktail_vault/frontend/src/components/CocktailDetailsPage.js
@@ -1,53 +1,10 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+import { useApi } from "../hooks/use-api";
 import { ingredientToString } from "../utils";
+import Navbar from "./Navbar";
 import "./CocktailDetailsPage.css";
-
-const ingredients = [
-  {
-    ingredient: {
-      id: 0,
-      name: "Rum",
-      hasKind: true,
-      kind: "Dark",
-      image: "https://picsum.photos/200",
-    },
-    amount: "1.5oz",
-  },
-  {
-    ingredient: {
-      id: 1,
-      name: "Rum",
-      hasKind: true,
-      kind: "Light",
-      image: "https://picsum.photos/200",
-    },
-    amount: "1.7oz",
-  },
-  {
-    ingredient: {
-      id: 2,
-      name: "Apple",
-      hasKind: false,
-      kind: "",
-      image: "https://picsum.photos/200",
-    },
-    amount: "1",
-  },
-];
-
-const instructions =
-  "Fill a highball glass with ice cubes. Add all ingredients. Garnish with lime and a cherry.";
-
-const image = "https://picsum.photos/500";
-
-const cocktail = {
-  id: 0,
-  name: "Rusty Nail",
-  ingredients,
-  instructions,
-  image,
-};
+import { useAuth0 } from "@auth0/auth0-react";
 
 function ListItem({ name, amount }) {
   return (
@@ -58,13 +15,13 @@ function ListItem({ name, amount }) {
   );
 }
 
-function IngredientsList({ title, ingredients }) {
+function IngredientsList({ title, cocktailIngredients }) {
   return (
     <div className="CocktailDetailsPage-ingredientsList">
       <h3 className="CocktailDetailsPage-listTitle">{title}</h3>
       <hr />
       <ul className="CocktailDetailsPage-list">
-        {ingredients.map((item) => (
+        {cocktailIngredients.map((item) => (
           <ListItem
             name={ingredientToString(item.ingredient)}
             amount={item.amount}
@@ -77,32 +34,85 @@ function IngredientsList({ title, ingredients }) {
 }
 
 function CocktailDetailsPage() {
-  return (
-    <div className="CocktailDetailsPage">
-      <Navbar />
-      <div className="container">
-        <h1 className="CocktailDetailsPage-title">{cocktail.name}</h1>
-        <IngredientsList
-          title="Ingredients I don't have"
-          ingredients={cocktail.ingredients}
-        />
-        <IngredientsList
-          title="Ingredients I don't have"
-          ingredients={cocktail.ingredients}
-        />
-        <h3 className="CocktailDetailsPage-listTitle">How to make</h3>
-        <hr />
-        <p className="CocktailDetailsPage-instructions">
-          {cocktail.instructions}
-        </p>
-        <h3 className="CocktailDetailsPage-listTitle">Images</h3>
-        <hr />
-        <div className="CocktailDetailsPage-images">
-          <img src={cocktail.image} alt={cocktail.name} />
+  const { cocktailId } = useParams();
+  const { isAuthenticated } = useAuth0();
+  const {
+    loading,
+    data: cocktail,
+    setUrl,
+  } = useApi(`http://localhost:8000/api/get_cocktail?id=${cocktailId}`, true);
+
+  useEffect(() => {
+    if (isAuthenticated)
+      setUrl(
+        `http://localhost:8000/api/get_cocktail_with_possessions?id=${cocktailId}`
+      );
+    else setUrl(`http://localhost:8000/api/get_cocktail?id=${cocktailId}`);
+  }, [isAuthenticated]);
+
+  if (cocktail)
+    return (
+      <div className="CocktailDetailsPage">
+        <Navbar />
+        <div className="container">
+          <h1 className="CocktailDetailsPage-title">{cocktail.name}</h1>
+          {"possessed" in cocktail ? (
+            <>
+              <IngredientsList
+                title="Ingredients I have"
+                cocktailIngredients={cocktail.cocktailIngredients.filter(
+                  (cocktailIngredient) =>
+                    cocktail.possessed.includes(
+                      cocktailIngredient.ingredient.id
+                    )
+                )}
+              />
+              <IngredientsList
+                title="Ingredients I don't have"
+                cocktailIngredients={cocktail.cocktailIngredients.filter(
+                  (cocktailIngredient) =>
+                    !cocktail.possessed.includes(
+                      cocktailIngredient.ingredient.id
+                    )
+                )}
+              />
+            </>
+          ) : (
+            <IngredientsList
+              title="Ingredients"
+              cocktailIngredients={cocktail.cocktailIngredients}
+            />
+          )}
+          <h3 className="CocktailDetailsPage-listTitle">How to make</h3>
+          <hr />
+          <p className="CocktailDetailsPage-instructions">
+            {cocktail.instructions}
+          </p>
+          <h3 className="CocktailDetailsPage-listTitle">Images</h3>
+          <hr />
+          <div className="CocktailDetailsPage-images">
+            <img
+              src={`http://172.30.1.201:9000/resize?width=300&file=cocktails/${cocktail.id}/Image_1.jpg`}
+              alt={cocktail.name}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  else if (loading)
+    return (
+      <div className="CocktailDetailsPage">
+        <Navbar />
+        Loading...
+      </div>
+    );
+  else
+    return (
+      <div className="CocktailDetailsPage">
+        <Navbar />
+        Recipe not found.
+      </div>
+    );
 }
 
 export default CocktailDetailsPage;
